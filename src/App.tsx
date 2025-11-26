72e2b024fed9b909ca5ce8b3970f6674f5f0a8cf
Anthony Paredes <anthonyparedes1080@gmail.com>
2025-11-26 19:10:31 +0000

    Guide 13

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 import { Grid } from '@mui/material'
+import HeaderUI from './components/HeaderUI';
+import AlertUI from './components/AlertUI';
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <Grid container spacing={5} justifyContent="center" alignItems="center">
 
       {/* Encabezado */}
-      <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+      <Grid size={{ xs: 12, md: 12 }}><HeaderUI/>Elemento: Encabezado</Grid>
 
       {/* Alertas */}
-      <Grid size={12}>Elemento: Alertas</Grid>
+      <Grid size={12} container justifyContent="right" alignItems="center"><AlertUI description="No se preveen lluvias"/>Elemento: Alertas</Grid>
 
       {/* Selector */}
       <Grid size={{ xs: 12, md: 3  }}>Elemento: Selector</Grid>
