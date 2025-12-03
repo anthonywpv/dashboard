7e654141bc734857521b4987c7ba552ec47ef749
Anthony Paredes <anthonyparedes1080@gmail.com>
2025-12-03 19:42:58 +0000

    Guide 15

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,52 @@ import { Grid } from '@mui/material'
 import HeaderUI from './components/HeaderUI';
 import AlertUI from './components/AlertUI';
 import SelectorUI from './components/SelectorUI';
+import IndicatorUI from './components/IndicatorUI';
+import useFetchData from './functions/useFetchData';
 
 function App() {
   //const [count, setCount] = useState(0)
+  const dataFetcherOutput = useFetchData();
 
   return (
     <Grid container spacing={5} justifyContent="center" alignItems="center">
 
       {/* Encabezado */}
-      <Grid size={{ xs: 12, md: 12 }}><HeaderUI/>Elemento: Encabezado</Grid>
+      <Grid size={{ xs: 12, md: 12 }}><HeaderUI />Elemento: Encabezado</Grid>
 
       {/* Alertas */}
-      <Grid size={12} container justifyContent="right" alignItems="center"><AlertUI description="No se preveen lluvias"/>Elemento: Alertas</Grid>
+      <Grid size={12} container justifyContent="right" alignItems="center"><AlertUI description="No se preveen lluvias" />Elemento: Alertas</Grid>
 
       {/* Selector */}
-      <Grid size={{ xs: 12, md: 3  }}><SelectorUI/>Elemento: Selector</Grid>
+      <Grid size={{ xs: 12, md: 3 }}><SelectorUI />Elemento: Selector</Grid>
 
       {/* Indicadores */}
-      <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
+      <Grid container size={{ xs: 12, md: 9 }} >
+
+        <Grid size={{ xs: 12, md: 3 }}>
+          {dataFetcherOutput && <IndicatorUI title='Temperatura (2m)' description={`${dataFetcherOutput.current.temperature_2m} ${dataFetcherOutput.current_units.temperature_2m}`} />
+          }</Grid>
+
+        <Grid size={{ xs: 12, md: 3 }}>
+          {dataFetcherOutput && <IndicatorUI title='Temperatura Aparente' description={`${dataFetcherOutput.current.apparent_temperature} ${dataFetcherOutput.current_units.apparent_temperature}`} />
+          }</Grid>
+
+        <Grid size={{ xs: 12, md: 3 }}>
+          {dataFetcherOutput && <IndicatorUI title='Humedad Relativa (2m)' description={`${dataFetcherOutput.current.relative_humidity_2m} ${dataFetcherOutput.current_units.relative_humidity_2m}`} />
+          }</Grid>
+
+        <Grid size={{ xs: 12, md: 3 }}>
+          {dataFetcherOutput && <IndicatorUI title='Velocidad del Viento (10m)' description={`${dataFetcherOutput.current.wind_speed_10m} ${dataFetcherOutput.current_units.wind_speed_10m}`} />
+          }</Grid>
+      </Grid>
 
       {/* Gráfico */}
       <Grid
-      sx={{ display: { xs: "none", md: "block"} }}>Elemento: Gráfico</Grid>
+        sx={{ display: { xs: "none", md: "block" } }}>Elemento: Gráfico</Grid>
 
       {/* Tabla */}
       <Grid
-      sx={{ display: { xs: "none", md: "block" } }}
+        sx={{ display: { xs: "none", md: "block" } }}
       >Elemento: Tabla</Grid>
 
       {/* Información adicional */}
---
+++ src/functions/useFetchData.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import { type OpenMeteoResponse } from '../types/DashboardTypes';
+
+export default function useFetchData(): OpenMeteoResponse {
+    const URL = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current=temperature_2m,relative_humidity_2m,apparent_temperature,wind_speed_10m&timezone=America%2FChicago';
+
+    const [data, setData] = useState<OpenMeteoResponse | null>();
+
+    useEffect(() => {
+            const fetchData = async () => {
+      try {
+        const response = await fetch(URL);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const json: OpenMeteoResponse = await response.json(); 
+        setData(json); 
+      } catch (error) {
+        console.error("Fetching data failed", error);
+      }
+    };
+    fetchData();
+     }, []); // El array vacío asegura que el efecto se ejecute solo una vez después del primer renderizado
+
+    
+    return data;
+
+}
